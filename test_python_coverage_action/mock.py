f3d6d55239bc238a75d8d168113eaab3824bf986
Matias Arola <arola@veristable.com>
2022-06-13 09:30:54 +0300

    feat: initial commit

---
+++ test_python_coverage_action/mock.py
@@ -0,0 +1,5 @@
+def mock_function():
+  return 3
+
+def mock_function2():
+  return 14159
---
+++ tests/test_mock.py
@@ -0,0 +1,5 @@
+from test_python_coverage_action import mock_function
+
+class TestMock:
+    def test_mock(self):
+        assert mock_function() == 3
